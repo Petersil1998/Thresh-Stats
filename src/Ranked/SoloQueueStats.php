a994eefa800950f17e680a5f8a1c65225d09c271
Petersil1998 <silbernagl.peter@rolmail.net>
2020-08-31 20:41:27 +0200

    Testing

---
+++ src/Ranked/SoloQueueStats.php
@@ -0,0 +1,16 @@
+<?php
+
+
+class SoloQueueStats
+{
+    /**
+     * Gathers statistics for Solo/Duo Queue for the given Rank
+     * @see \Thresh\Constants\RankedDivisions
+     * @see \Thresh\Constants\RankedTiers
+     * @param $division
+     * @param $tier
+     */
+    public static function getLeagueStats($tier, $division){
+        return "Test";
+    }
+}
